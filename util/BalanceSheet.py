61c2b257dfab48a550f122e9179f7e352f7bbae3
torkjel1 <120417207+torkjel1@users.noreply.github.com>
2023-08-27 17:31:35 +0200

    Delete util directory

--- util/BalanceSheet.py
+++
@@ -1,7 +0,0 @@
-from pandas import DataFrame
-
-class BalanceSheet:
-
-    def __init__(self, unit: str, balance_sheet: DataFrame):
-        self.unit = unit
-        self.balance_sheet = balance_sheet
--- util/CashFlow.py
+++
@@ -1,9 +0,0 @@
-from pandas import DataFrame
-
-
-class CashFlow:
-
-    def __init__(self, unit: str, cashflow: DataFrame):
-        self.unit = unit
-        self.cashflow = cashflow
-
